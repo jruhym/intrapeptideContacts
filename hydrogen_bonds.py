79ae304df2e2ad14f9becff68a124376bf129bf5
drew <a.heim@usciences.edu>
2013-03-14 23:16:53 -0400

    changed name of attribute of HBondGroup.___atom_str_list to .__atom_str_tuple Added asserts to its corresponding set method to assert it is a tuple of strs.

--- hydrogen_bonds.py
+++ hydrogen_bonds.py
@@ -2,17 +2,19 @@ class HBondGroup():
 
 	def __init__(self):
 		self.__valence__ = 'Not yet set'
-		self.__acceptor_atoms_str_list__ = ['Not yet set']
-		self.__donor_atoms_str_list__ = ['Not yet set']
+		self.__acceptor_atoms_str_tupl__ = ['Not yet set']
+		self.__donor_atoms_str_tupl__ = ['Not yet set']
 		self.__residue__ = 'Not yet set'
 		self.__H_bond_radius__ = 'Not yet set'
 		self.__max_num_H_bonds__ = 'Not yet set'
+		self.__NN__ = 'Not yet set'
+		self.__NNN__ = 'Not yet set'
 
 	def get_valance(self):
 		return self.__valence__
 
-	def get_atoms_str_list(self):
-		return self.__donor_atoms_str_list__
+	def get_atoms_str_tupl(self):
+		return self.__donor_atoms_str_tupl__
 
 	def get_residue(self):
 		return self.__residue__
@@ -22,13 +24,22 @@ class HBondGroup():
 
 	def get_max_num_H_bonds(self):
 		return self.__max_num_H_bonds__
+
+	def get_NN(self):
+		return self.__NN__
+
+	def get_NNN(self):
+		return self.__NNN__
 	
 	def set_valance(self, valence):
 		self.__valence__ = valence
 
-	def set__atoms_str_list(self, _atoms_str_list):
-		#bi.TestListOfStr.setUp(_atoms_str_list)
-		self.___atoms_str_list__ = _atoms_str_list
+	def set_atoms_str_tupl(self, atoms_str_tupl):
+		assert type(atom_str_tupl) == tuple
+		assert len(atoms_str_tupl) > 0
+		for atom_str in atoms_str_tupl:
+			assert type(atom_str) == str
+		self.__atoms_str_tupl__ = atoms_str_tupl
 
 	def set_residue(self, residue):
 		assert type(residue) == str
@@ -47,6 +58,14 @@ class HBondGroup():
 		assert max_num_H_bonds < 3
 		self.__max_num_H_bonds__ = max_num_H_bonds
 
+	def set_NN(self, NN):
+		assert type(NN) == str
+		self.__NN__ = NN
+
+	def set_NNN(self, NNN):
+		assert type(NNN) == str
+		self.__NNN__ = NNN
+
 
 #class atom_IQ():
 
@@ -55,14 +74,16 @@ donor_Nsp2_I_peptide = HBondGroup()
 donor_Nsp2_I_peptide.set_valance("sp2")
 donor_Nsp2_I_peptide.set_residue("peptide")
 donor_Nsp2_I_peptide.set_H_bondonor_radius(1.90)
-donor_Nsp2_I_peptide.set__atoms_str_list(["NE1"])
+donor_Nsp2_I_peptide.set_atoms_str_tupl(["NE1"])
 donor_Nsp2_I_peptide.set_max_num_H_bonds(1)
+donor_Nsp2_I_peptide.set_NN("CA")
+donor_Nsp2_I_peptide.set_NNN("C")
 
 donor_Nsp2_I_Trp = HBondGroup()
 donor_Nsp2_I_Trp.set_valance("sp2")
 donor_Nsp2_I_Trp.set_residue("Trp")
 donor_Nsp2_I_Trp.set_H_bondonor_radius(1.90)
-donor_Nsp2_I_Trp.set__atoms_str_list(["NE1"])
+donor_Nsp2_I_Trp.set_atoms_str_tupl(["NE1"])
 donor_Nsp2_I_Trp.set_max_num_H_bonds(1)
 
 
