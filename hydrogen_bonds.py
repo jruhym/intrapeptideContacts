a68e5c7d6e63d614650bb9fc8eefc2933d1d002c
Leeroy Jenkins <jruhym@hotmail.com>
2013-03-18 18:44:05 -0400

    Added some classes for future use

--- hydrogen_bonds.py
+++ hydrogen_bonds.py
@@ -67,7 +67,8 @@ class HBondGroup():
 		self.__NNN__ = NNN
 
 
-#These objects comes from Stickle et al.
+# These objects come from Table 1 from Stickle et al. 
+# DOI: 10.1016/0022-2836(92)91058-W
 donor_Nsp2_I_peptide = HBondGroup()
 donor_Nsp2_I_peptide.set_valance("sp2")
 donor_Nsp2_I_peptide.set_residue("peptide")
@@ -84,5 +85,63 @@ donor_Nsp2_I_Trp.set_H_bondonor_radius(1.90)
 donor_Nsp2_I_Trp.set_atoms_str_tupl(["NE1"])
 donor_Nsp2_I_Trp.set_max_num_H_bonds(1)
 
+
+class AtomIQ():
+
+	def __init__(self):
+		# We may be able to set these at init automatically. 
+		self.__residue__ = 'Not Yet Set'
+		self.__name__ = 'Not Yet Set'
+		#self.__is_donor__ = -1
+		#self.__is_acceptor = -1
+		self.__valence__ = 'Not Yet Set'
+		self.__coordinates__ = [0.,0.,0.]
+
+	def get_residue(self):
+		return self.__residue__
+	def get_name(self):
+		return self.__name__
+	def get_is_donor(self):
+		return self.__is_donor__
+	def get_is_acceptor(self):
+		return self.__is_acceptor__
+	def get_coordinates(self):
+		return self.__coordinates__
+
+	def set_residue(self, residue):
+		assert type(residue) == str
+		self.__residue__ = residue
+	def set_name(self, name):
+		assert type(name) == str
+		self.__name__ = name
+	def set_is_donor(self, is_donor):
+		assert type(is_donor) == bool  
+		self.__is_donor__ = is_donor
+	def set_is_acceptor(self, is_acceptor):
+		assert type(is_acceptor) == bool  
+		self.__is_acceptor__ = is_acceptor
+	def set_coordinates(self, coordinates):
+		assert type(coordinates) == list
+		for x_i in coordinates:
+			assert type(x_i) == float
+		self.__coordinates__ = coordinates
+
+class DonorIQ(AtomIQ):
+	pass
+
+
+class AcceptorIQ(AtomIQ):
+	pass
+
+
+class BondIQ(object):
+	pass
+
+
+class HBondIQ(BondIQ):
+	pass
+
+
+class 
 
 
