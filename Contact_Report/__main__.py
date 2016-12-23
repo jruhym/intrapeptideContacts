9f5f891e0c3bcff4e91a63da4ddacb922a282b14
Andrew Heim <jruhym@gmail.com>
2016-12-22 22:48:12 -0500

    Fix errors in main

--- Contact_Report/__main__.py
+++ Contact_Report/__main__.py
@@ -1,21 +1,22 @@
 
 import sys
 import glob
-from .hydrogen_bonds import *
+from hydrogen_bonds import *
 
 for currentWildcard in sys.argv[1:]:
-	for file_path in glob.glob(currentWildcard):
-		reader = PDBATOMFileReader(file_path)
-		donorDict = {}
-		acceptorDict = {}
-		atoms = {}
-		for atom in reader:
-			atoms[atom.serial] = atom
-			if atom.participant != False:
-				if atom.participant.is_donor:
-					donorDict[atom.serial] = atom
-				if atom.participant.is_acceptor:
-					acceptorDict[atom.serial] = atom
-for donor in donorDict.itervalues():
-	for acceptor in acceptorDict.itervalues():
-        
+    for file_path in glob.glob(currentWildcard):
+        reader = PDBATOMFileReader(file_path)
+        donorDict = {}
+        acceptorDict = {}
+        atoms = {}
+        for atom in reader:
+            atoms[atom.serial] = atom
+            if atom.participant:
+                if atom.participant.is_donor:
+                    donorDict[atom.serial] = atom
+                if atom.participant.is_acceptor:
+                    acceptorDict[atom.serial] = atom
+        for donor in donorDict.itervalues():
+            for acceptor in acceptorDict.itervalues():
+                pass
+            pass
